def6e6e7698666aef603d4b2d6e71ed0e829d453
Sofia Peshekhonova <Peshehonova94@yandex.ru>
2023-06-27 10:01:31 +0300

    /Favourites

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,11 +1,23 @@
 function Header() {
   return (
+    //<header className="header">
     <header className="header header--authorized">
       <div className="container">
         <div className="header__inner">
+          {/* <a className="header__logo" href="index.html" aria-label="Переход на главную">
+              <img src="img/svg/logo.svg" width="170" height="69" alt="Кондитерская кекс" />
+            </a> */}
           <span className="header__logo">
             <img src="img/svg/logo.svg" width="170" height="69" alt="Кондитерская кекс" />
           </span>
+          {/* <div className="header__buttons">
+              <div className="header__btn">
+                <a className="btn btn--third header__link header__link--reg" href="register-page.html">Регистрация</a>
+              </div>
+              <div className="header__btn">
+                <a className="btn" href="login-page.html">Войти</a>
+              </div>
+            </div> */}
           <div className="header__user-info-wrap">
             <div className="header__user-info">
               <div className="header__user-avatar">
